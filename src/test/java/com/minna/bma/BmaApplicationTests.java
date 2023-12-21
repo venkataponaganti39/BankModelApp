325b930eef6b031fa8a70e86e5fc38cd28a4eb4c
venkataponaganti39 <154423599+venkataponaganti39@users.noreply.github.com>
2023-12-21 17:24:13 +0530

    Junit test case update

--- src/test/java/com/minna/bma/BmaApplicationTests.java
+++ src/test/java/com/minna/bma/BmaApplicationTests.java
@@ -2,6 +2,7 @@ package com.minna.bma;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minna.bma.entity.BankAccount;
+import com.minna.bma.entity.Transaction;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -38,6 +40,32 @@ public class BmaApplicationTests {
 	public void setup() {
 		restTemplate = new TestRestTemplate();
 		headers = new HttpHeaders();
+	}
+
+	@Test
+	public void getBankAccountsByTransactionAmount() throws Exception {
+		BankAccount account = new BankAccount();
+		account.setIdentifier(UUID.randomUUID());
+		account.setUserIdentifier(UUID.randomUUID());
+
+		HttpEntity<BankAccount> request = new HttpEntity<>(account, headers);
+		ResponseEntity<BankAccount> response = restTemplate.exchange(createURLWithPort("/api/bank-accounts"),
+				HttpMethod.POST, request, BankAccount.class);
+
+		Transaction transaction = new Transaction();
+		transaction.setAmount(-400);
+		transaction.setBankAccount(new BankAccount(response.getBody().getUUID()));
+		transaction.setDate(LocalDateTime.now());
+		transaction.setReceiptNumber("12345");
+		transaction.setText("Gym");
+
+		HttpEntity<Transaction> req = new HttpEntity<>(transaction, headers);
+		restTemplate.exchange(createURLWithPort("/api/bank-accounts/" + response.getBody().getUUID() + "/transactions"),
+				HttpMethod.POST, req, Transaction.class);
+
+		mockMvc.perform(MockMvcRequestBuilders.get(createURLWithPort("/api/bank-accounts"))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
 	}
 
 	@Test
